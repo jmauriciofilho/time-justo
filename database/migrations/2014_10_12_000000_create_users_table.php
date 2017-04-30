7b19b6f3075ec044c51e72e6a8e8e911996af704
jmfcientista <jmfcientista@gmail.com>
2017-04-30 12:50:51 -0300

    remember token

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->rememberToken();
             $table->double('overall')->default(0.0);
             $table->integer('goalsScored')->default(0);
             $table->string('appearances')->nullable();
